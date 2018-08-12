332ad20bf144eb752499f07035e89819e93d5fc4
Redda20 <42320560+Redda20@users.noreply.github.com>
2018-08-12 18:54:02 +0100

    Add files via upload

---
+++ config_override.php
@@ -0,0 +1,8 @@
+<?php
+/***CONFIGURATOR***/
+$sugar_config['use_real_names'] = false;
+$sugar_config['stack_trace_errors'] = false;
+$sugar_config['developerMode'] = false;
+$sugar_config['disabled_themes'] = '';
+$sugar_config['default_module_favicon'] = false;
+/***CONFIGURATOR***/
